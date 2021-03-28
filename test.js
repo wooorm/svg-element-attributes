6ff91f73f50b3a10bd9c6ee5e09a957a3591cfaa
Titus Wormer <tituswormer@gmail.com>
2021-03-28 17:22:14 +0200

    Update dev-dependencies

--- test.js
+++ test.js
@@ -8,22 +8,22 @@ test('svgElementAttributes', function (t) {
   t.equal(typeof svgElementAttributes, 'object', 'should be an `object`')
 
   t.doesNotThrow(function () {
-    Object.keys(svgElementAttributes).forEach(function (name) {
+    for (const name of Object.keys(svgElementAttributes)) {
       assert.ok(Array.isArray(svgElementAttributes[name]), name)
-    })
+    }
   }, 'values should be array')
 
   t.doesNotThrow(function () {
-    Object.keys(svgElementAttributes).forEach(function (name) {
+    for (const name of Object.keys(svgElementAttributes)) {
       var props = svgElementAttributes[name]
 
-      props.forEach(function (prop) {
+      for (const prop of props) {
         var label = prop + ' in ' + name
         assert.ok(typeof prop, 'string', label + ' should be string')
         assert.strictEqual(prop, prop.trim(), label + ' should be trimmed')
         assert.ok(/^[a-z][a-z\d-]*$/i.test(prop), label + ' should be `a-z-`')
-      })
-    })
+      }
+    }
   }, 'name should be lower-case, alphabetical strings')
 
   t.end()
