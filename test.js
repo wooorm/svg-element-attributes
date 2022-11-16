45bd1718ea089f43fa8474d9f94fda62133bc7e7
Titus Wormer <tituswormer@gmail.com>
2022-11-16 15:16:33 +0100

    Use Node test runner

--- test.js
+++ test.js
@@ -1,53 +1,44 @@
-import assert from 'node:assert'
-import test from 'tape'
+import assert from 'node:assert/strict'
+import test from 'node:test'
 import {svgElementAttributes} from './index.js'
 
 const own = {}.hasOwnProperty
 
-test('svgElementAttributes', function (t) {
-  t.equal(typeof svgElementAttributes, 'object', 'should be an `object`')
+test('svgElementAttributes', function () {
+  assert.equal(typeof svgElementAttributes, 'object', 'should be an `object`')
 
-  t.doesNotThrow(function () {
-    /** @type {string} */
-    let tagName
+  /** @type {string} */
+  let tagName
 
-    for (tagName in svgElementAttributes) {
-      if (own.call(svgElementAttributes, tagName)) {
-        assert.ok(Array.isArray(svgElementAttributes[tagName]), tagName)
+  for (tagName in svgElementAttributes) {
+    if (own.call(svgElementAttributes, tagName)) {
+      assert.ok(Array.isArray(svgElementAttributes[tagName]), tagName)
+    }
+  }
+
+  for (tagName in svgElementAttributes) {
+    if (own.call(svgElementAttributes, tagName)) {
+      const attributes = svgElementAttributes[tagName]
+      let index = -1
+
+      while (++index < attributes.length) {
+        const attribute = attributes[index]
+        const label = attribute + ' in ' + tagName
+        assert.strictEqual(
+          typeof attribute,
+          'string',
+          label + ' should be string'
+        )
+        assert.strictEqual(
+          attribute,
+          attribute.trim(),
+          label + ' should be trimmed'
+        )
+        assert.ok(
+          /^[a-z][a-z\d-]*$/i.test(attribute),
+          label + ' should be `a-z-`'
+        )
       }
     }
-  }, 'values should be array')
-
-  t.doesNotThrow(function () {
-    /** @type {string} */
-    let tagName
-
-    for (tagName in svgElementAttributes) {
-      if (own.call(svgElementAttributes, tagName)) {
-        const attributes = svgElementAttributes[tagName]
-        let index = -1
-
-        while (++index < attributes.length) {
-          const attribute = attributes[index]
-          const label = attribute + ' in ' + tagName
-          assert.strictEqual(
-            typeof attribute,
-            'string',
-            label + ' should be string'
-          )
-          assert.strictEqual(
-            attribute,
-            attribute.trim(),
-            label + ' should be trimmed'
-          )
-          assert.ok(
-            /^[a-z][a-z\d-]*$/i.test(attribute),
-            label + ' should be `a-z-`'
-          )
-        }
-      }
-    }
-  }, 'name should be lowercase, alphabetical strings')
-
-  t.end()
+  }
 })
